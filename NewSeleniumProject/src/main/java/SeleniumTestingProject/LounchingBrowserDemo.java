a2c0d8ff05979239bff754a9b6adb1a7d66c0583
Rakesh_mishra <119526352+rakesh-mishra5719@users.noreply.github.com>
2024-01-05 22:41:18 +0530

    Add files via upload

---
+++ NewSeleniumProject/src/main/java/SeleniumTestingProject/LounchingBrowserDemo.java
@@ -0,0 +1,21 @@
+package SeleniumTestingProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class LounchingBrowserDemo {
+
+	public static void main(String[] args) {
+		
+		// launching Web Driver
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://leetcode.com/");
+		
+		
+		
+
+	}
+
+}
